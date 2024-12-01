86504db98c2bebe1348b3f0982f8985e663fff39
soujanyav <155467921+soujanya-bit@users.noreply.github.com>
2024-12-01 22:16:42 +0530

    Add files via upload

---
+++ src/components/Layout/index.js
@@ -0,0 +1,23 @@
+import { Outlet } from 'react-router-dom'
+import Sidebar from '../Sidebar/'
+import './index.scss'
+
+const Layout = () => {
+  return (
+    <div className="App">
+      <Sidebar />
+      <div className="page">
+        <span className="tags top-tags">&lt;&gt;</span>
+
+        <Outlet />
+        <span className="tags bottom-tags">
+          &lt;&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;&gt;</span>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Layout
